de4f5025967049e95ca65adf91291e7888612d6b
rahulhundekari <rahuld.hundekari@gmail.com>
2024-06-06 10:25:09 +0530

    Initial Commit of code

---
+++ app/src/main/java/com/example/pas/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.example.pas.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+
+
+}
